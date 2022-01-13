8a8614ac052a70cbb76b36e11fc04df60860ecdd
ASPAnas <pananeo20@gmail.com>
2022-01-13 16:24:57 +0100

    Add index page.

--- placesflection/placesflection/settings.py
+++ placesflection/placesflection/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "placesflection.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "placeflection" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- placesflection/placesflection/urls.py
+++ placesflection/placesflection/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="placeflection/index.html")),
     path("memories/", include("memories.urls")),
     path("admin/", admin.site.urls),
 ]
